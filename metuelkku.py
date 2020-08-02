22e5594fe09da1ca7135a579349244a932b88b3e
Jerry <69111819+KCitymarket@users.noreply.github.com>
2020-08-02 22:58:37 +0300

    Update metuelkku.py

--- metuelkku.py
+++ metuelkku.py
@@ -1,20 +1,28 @@
 import time
 import random
 
+#User chooses a number between 1 and 1000 
+#machine outputs random numbers until it gets user input
+#then it outputs "Here's your number:(user input) it took (number of tries) tries"
 
-#Shoots random numbers at you until you get 50 and then prints how many tries it took
-
-
-def trolli():
+def numRand():
+    num = int(input("Minkä numeron haluat arvottavaksi (väliltä 1 - 1000)"))    
+    if num > 1000:
+        print("liian suuri numero")
+        numRand()
+    if num < 0:
+        print("liian pieni numero")
+        numRand()
     yritykset = 0
     luku = 0
-    while luku != 50:
-        luku = (random.randint(0, 100))
+    while luku != num:
+        luku = (random.randint(0, 1000))
         yritykset = yritykset + 1
         print(luku)
-        if luku == 50:
-            print ("jes tos on:", luku, "siinä meni", yritykset, "kertaa")
+        if luku == num:
+            print ("Siinä on numerosi:", luku, "Siinä meni", yritykset, "yritystä")
             time.sleep(3)
+            break
 
 
-trolli()
+numRand()
